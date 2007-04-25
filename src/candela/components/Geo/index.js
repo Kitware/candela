836dc9c6800c9979b02a1b6055392165f73eb237
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2007-04-25 01:01:01 -0700

    Added point data layer

--- src/candela/components/Geo/index.js
+++ src/candela/components/Geo/index.js
@@ -2,25 +2,41 @@ import spec from './spec.json';
 import $ from 'jquery';
 import geojs from 'geojs/geo.js';
 import VisComponent from '../../VisComponent';
-//import geojsext from 'geojs/geo.ext.min.js';
 
 export default class Geo extends VisComponent {
   static get spec () {
     return {
       options: [
+        {
+          name: 'data',
+          type: 'table',
+          format: 'objectlist'
+        }
       ]
     };
   }
 
   constructor (el, options) {
     super(el);
-    // this.chart = vega.chart(spec, el, options);
+    console.log(options);
     this.plot = geojs.map({
                   node: el,
                   zoom: 6,
                   center: {x: 28.9550, y: 41.0136}
                 });
     this.plot.createLayer('osm');
+    if (options.features) {
+      for(let i = 0; i < options.features.length; ++i) {
+        this.plot.createLayer('feature')
+          .createFeature(options.features[i].type)
+            .data(options.features[i].data)
+            .position(function(d) {
+              return {x: d[options.features[i].position.x],
+                      y: d[options.features[i].position.y]}
+            });
+      }
+      this.plot.draw();
+    }
   }
 
   render () {
