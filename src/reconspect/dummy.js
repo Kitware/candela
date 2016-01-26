e705d0519bc591bd5f1b1dbeb94ccf33656f7d34
Roni Choudhury <roni.choudhury@kitware.com>
2016-01-26 15:58:34 -0500

    Add dummy visualization component (along with visualization namespace)

---
+++ src/reconspect/dummy.js
@@ -0,0 +1,19 @@
+(function (reconspect, d3) {
+  reconspect.dummy = {
+    construct: function (div, data) {
+      var d = d3.select(div);
+
+      d = d.append('ul');
+      d.selectAll('li')
+        .data(data)
+        .enter()
+        .append('li')
+        .text(function (d) {
+          return d.text;
+        })
+      .style(function (d) {
+        return d.color;
+      });
+    }
+  };
+}(window.reconspect, window.d3));
