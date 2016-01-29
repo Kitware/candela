83e6702ed3f14250093b1798a9b07d58da56b5d8
Roni Choudhury <roni.choudhury@kitware.com>
2016-01-29 09:00:17 -0500

    Add base webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+var webpack = require('webpack');
+var production = process.env.NODE_ENV === 'production';
+
+var plugins = [
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'main',
+    children: true,
+    minChunks: 2
+  })
+];
+
+if (production) {
+  plugins = plugins.concat([
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.MinChunkSizePlugin({
+        minChunkSize: 51200,
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: true,
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.DefinePlugin({
+        __SERVER__: !production,
+        __DEVELOPMENT__: !production,
+        __DEVTOOLS__: !production,
+        'process.env': {
+          BABEL_ENV: JSON.stringify(process.env.NODE_ENV)
+        }
+      })
+  ]);
+}
+
+module.exports = {
+  debug: !production,
+  devtool: production ? false : 'eval',
+  entry: './src',
+  output: {
+    path: 'dist',
+    filename: 'resplendent.js',
+    libraryTarget: 'umd',
+    publicPath: 'dist/'
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        },
+        include: __dirname + '/src'
+      },
+      {
+        test: /\.scss/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.html/,
+        loader: 'html'
+      }
+    ]
+  }
+};
