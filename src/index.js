883eaaf61923f998638afd65f006657caaf320d8
Roni Choudhury <roni.choudhury@kitware.com>
2016-02-10 13:37:33 -0500

    Add some explanatory comments

--- src/index.js
+++ src/index.js
@@ -51,6 +51,8 @@ function linechart () {
   let vis = new LineChart(el, data);
   vis.render();
 
+  // Every second, add a random data point to the chart, and remove the oldest
+  // data point.
   let counter = data.length;
   window.setInterval(function () {
     data.push([counter, Math.random() * 10]);
@@ -62,6 +64,7 @@ function linechart () {
     vis.render();
   }, 1000);
 
+  // Whenever the component renders, change the number of updates in the DOM.
   vis.on('render', function () {
     $('#counter').text(counter - data.length);
   });
