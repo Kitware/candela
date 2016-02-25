3525b5b04c44fcc13025e70c9bde43b0f70de182
Roni Choudhury <roni.choudhury@kitware.com>
2016-02-25 13:32:05 -0500

    Import datasets from Girder

--- app/resonant-reference-app/core_views/mainPage/overlay/datasetLibrary/datasetLibrary.js
+++ app/resonant-reference-app/core_views/mainPage/overlay/datasetLibrary/datasetLibrary.js
@@ -6,43 +6,58 @@ var Backbone = require('backbone'),
     d3 = require('d3'),
     Dataset = require('../../../../core_models/dataset.js'),
     myTemplate = require('./template.html'),
-    library = require('./datasetLibrary.json'),
-    libImage = require('../../../../assets/images/library.svg');
+    libImage = require('../../../../assets/images/library.svg'),
+    girder = window.girder;
 
 var DatasetLibrary = Backbone.View.extend({
     render : function () {
         var self = this;
         self.$el.html(myTemplate);
-        
-        var libraryButtons = d3.select('div.datasetLibrary')
-            .selectAll('.circleButton')
-            .data(library);
 
-        var libraryButtonsEnter = libraryButtons.enter().append('div')
-            .attr('class', 'circleButton');
-        libraryButtons.exit().remove();
+        girder.restRequest({
+          path: 'resource/lookup?path=/collection/ReferenceApp/Data',
+          type: 'GET',
+          error: null
+        }).done(function (folder) {
+          var datasets = new girder.collections.ItemCollection();
+          datasets.pageLimit = 100;
+          datasets.fetch({
+            folderId: folder._id
+          });
 
-        libraryButtonsEnter.append('img');
-        libraryButtons.selectAll('img')
-            .attr('src', libImage);
-        
-        libraryButtonsEnter.append('span');
-        libraryButtons.selectAll('span')
-            .text(function (d) { return d; });
+          datasets.on('reset', function (items) {
+            var libraryButtons = d3.select('div.datasetLibrary')
+              .selectAll('.circleButton')
+              .data(items.models);
 
-        d3.select('div.libraryInterface').selectAll('.circleButton')
-            .on('click', function (d) {
-                window.user.addDataset({
-                    name : 'd',
-                    attrs : {
-                        'this' : Dataset.DATA_TYPES.STRING,
-                        'is' : Dataset.DATA_TYPES.STRING
-                    },
-                    contents : 'this,is\na,test',
-                    locationType : Dataset.LOCATION_TYPES.LIBRARY
+
+            var libraryButtonsEnter = libraryButtons.enter().append('div')
+              .attr('class', 'circleButton');
+            libraryButtons.exit().remove();
+
+            libraryButtonsEnter.append('img');
+            libraryButtons.selectAll('img')
+              .attr('src', libImage);
+
+            libraryButtonsEnter.append('span');
+            libraryButtons.selectAll('span')
+              .text(function (d) { return d.get('name'); });
+
+            d3.select('div.libraryInterface').selectAll('.circleButton')
+              .on('click', function (d) {
+                girder.restRequest({
+                  path: 'item/' + d.id + '/download',
+                  type: 'GET',
+                  error: null,
+                  dataType: 'text'
+                }).done(function (data) {
+                  d.set('content', data);
+                  window.user.addDataset(d);
+                  window.overlay.render(null);
                 });
-                window.overlay.render(null);
-            });
+              });
+          });
+        });
     }
 });
 
