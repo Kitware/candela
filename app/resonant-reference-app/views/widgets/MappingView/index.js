deff306984bfb46dec7d5ac494a7f2bcb18cfc53
Roni Choudhury <roni.choudhury@kitware.com>
2016-03-03 15:46:44 -0500

    Gather some information about field selectors

--- app/resonant-reference-app/views/widgets/MappingView/index.js
+++ app/resonant-reference-app/views/widgets/MappingView/index.js
@@ -1,10 +1,16 @@
 import Backbone from 'backbone';
 import myTemplate from './index.jade';
+import candela from '../../../../../src';
 
 let MappingView = Backbone.View.extend({
   initialize: function () {
   },
   render: function () {
+    const spec = candela.components.Scatter.options;
+    const fields = spec.filter((opt) => opt.selector && opt.selector[0] === 'field');
+
+    console.log(fields);
+
     this.$el.html(myTemplate());
   }
 });
