374f93c6c27c90d1f81abe2fd32df0aa626cf732
Roni Choudhury <roni.choudhury@kitware.com>
2016-03-03 16:32:04 -0500

    Ask for and report field mappings

--- app/resonant-reference-app/views/widgets/MappingView/index.js
+++ app/resonant-reference-app/views/widgets/MappingView/index.js
@@ -12,6 +12,15 @@ let MappingView = Backbone.View.extend({
     this.$el.html(myTemplate({
       fieldOpts: fields
     }));
+
+    this.$('.mapping-update').on('click', () => {
+      const names = this.$('.field').map((i, e) => ({
+        name: $(e).attr('name'),
+        val: $(e).val()
+      })).get();
+
+      console.log(names);
+    });
   }
 });
 
