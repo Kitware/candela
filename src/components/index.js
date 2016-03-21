dc7f24b71d8db976856dc5fa973c81ab58bef747
Jeffrey Baumes <jeff.baumes@kitware.com>
2016-03-21 15:39:14 -0400

    Add box plot

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
+import BoxPlot from './BoxPlot';
 import Histogram from './Histogram';
 import Scatter from './Scatter';
 import ScatterMatrix from './ScatterMatrix';
 
 export default {
+  BoxPlot,
   Histogram,
   Scatter,
   ScatterMatrix
