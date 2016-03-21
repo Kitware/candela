0d4f633c787c4cf1af33ad2bb9fb5d469f556e25
Roni Choudhury <roni.choudhury@kitware.com>
2016-03-21 09:53:08 -0400

    Remove core directory; place its substantive contents in candela top-level

--- src/candela/index.js
+++ src/candela/index.js
@@ -1,7 +1,22 @@
-import core from './core';
 import components from './components';
+import telegraph from 'telegraph-events';
 
-export default {
-  core,
+export class VisualizationComponent {
+  constructor (el) {
+    if (!el) {
+      throw new Error('"el" is a required argument');
+    }
+
+    this.el = el;
+
+    telegraph(this);
+  }
+
+  render () {
+    throw new Error('"refresh() is pure abstract"');
+  }
+}
+
+export {
   components
 };
