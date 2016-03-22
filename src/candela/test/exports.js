22eaf0d91d2332f28b61fc80386c031801be7936
Roni Choudhury <roni.choudhury@kitware.com>
2016-03-22 15:42:12 -0400

    Fix style error in exports test file

--- src/candela/test/exports.js
+++ src/candela/test/exports.js
@@ -8,24 +8,24 @@ test('Verify structure and content of exported library object', t => {
   t.ok(candela.VisComponent, 'candela.VisComponent exists');
 
   t.ok(candela.VisComponent.VisComponent, 'candela.VisComponent.VisComponent exists');
-  t.equal(typeof(candela.VisComponent.VisComponent), 'function', 'candela.VisComponent.VisComponent is a function');
+  t.equal(typeof candela.VisComponent.VisComponent, 'function', 'candela.VisComponent.VisComponent is a function');
 
   t.ok(candela.components, 'candela.components exists');
 
   t.ok(candela.components.Dummy, 'candela.components.Dummy exists');
-  t.equal(typeof(candela.components.Dummy), 'function', 'candela.components.Dummy is a function');
+  t.equal(typeof candela.components.Dummy, 'function', 'candela.components.Dummy is a function');
 
   t.ok(candela.components.Histogram, 'candela.components.Histogram exists');
-  t.equal(typeof(candela.components.Histogram), 'function', 'candela.components.Histogram is a function');
+  t.equal(typeof candela.components.Histogram, 'function', 'candela.components.Histogram is a function');
 
   t.ok(candela.components.ParallelCoordinates, 'candela.components.ParallelCoordinates exists');
-  t.equal(typeof(candela.components.ParallelCoordinates), 'function', 'candela.components.ParallelCoordinates is a function');
+  t.equal(typeof candela.components.ParallelCoordinates, 'function', 'candela.components.ParallelCoordinates is a function');
 
   t.ok(candela.components.Scatter, 'candela.components.Scatter exists');
-  t.equal(typeof(candela.components.Scatter), 'function', 'candela.components.Scatter is a function');
+  t.equal(typeof candela.components.Scatter, 'function', 'candela.components.Scatter is a function');
 
   t.ok(candela.components.ScatterMatrix, 'candela.components.ScatterMatrix exists');
-  t.equal(typeof(candela.components.ScatterMatrix), 'function', 'candela.components.ScatterMatrix is a function');
+  t.equal(typeof candela.components.ScatterMatrix, 'function', 'candela.components.ScatterMatrix is a function');
 
   t.end();
 });
