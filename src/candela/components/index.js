71d38812f07b387af72188926d91577b916e5bab
Roni Choudhury <roni.choudhury@kitware.com>
2016-03-22 13:41:26 -0400

    Add "exports" test
    Remove "dummy" test as it is no longer needed now

--- src/candela/components/index.js
+++ src/candela/components/index.js
@@ -1,9 +1,13 @@
+import Dummy from './Dummy';
 import Histogram from './Histogram';
+import ParallelCoordinates from './ParallelCoordinates';
 import Scatter from './Scatter';
 import ScatterMatrix from './ScatterMatrix';
 
 export default {
+  Dummy,
   Histogram,
+  ParallelCoordinates,
   Scatter,
   ScatterMatrix
 };
