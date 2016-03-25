b863421a7dbe53e5e561b18f7d01ef10f2df7a71
Jeffrey Baumes <jeff.baumes@kitware.com>
2016-03-25 14:56:05 -0400

    Adding LineUp component

--- src/candela/components/index.js
+++ src/candela/components/index.js
@@ -1,6 +1,7 @@
 import BoxPlot from './BoxPlot';
 import Dummy from './Dummy';
 import Histogram from './Histogram';
+import LineUp from './LineUp';
 import ParallelCoordinates from './ParallelCoordinates';
 import Scatter from './Scatter';
 import ScatterMatrix from './ScatterMatrix';
@@ -9,6 +10,7 @@ export default {
   BoxPlot,
   Dummy,
   Histogram,
+  LineUp,
   ParallelCoordinates,
   Scatter,
   ScatterMatrix
