9a9dec48335e3d8a438f68e13a61a97c7c4a2147
Roni Choudhury <roni.choudhury@kitware.com>
2016-03-30 14:34:20 -0400

    Add AutoResize mixin

---
+++ src/candela/VisComponent/mixin/AutoResize.js
@@ -0,0 +1,16 @@
+import Resize from './Resize';
+
+let AutoResize = Base => class extends Resize(Base) {
+  constructor (...args) {
+    super(...args);
+
+    this.on('resize', (w, h) => {
+      this.width = w;
+      this.height = h;
+
+      this.render();
+    });
+  }
+}
+
+export default AutoResize;
