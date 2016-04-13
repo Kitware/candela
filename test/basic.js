b1600d16370faee87afe862333fcf139e907ca6d
Roni Choudhury <roni.choudhury@kitware.com>
2016-04-13 13:13:13 -0400

    Add basic example of image extraction

--- test/basic.js
+++ test/basic.js
@@ -46,8 +46,42 @@ test('Google logo', t => {
   });
 });
 
+test('Google image comparison', t => {
+  let nightmare = Nightmare({
+    show: false
+  });
+
+  Promise.resolve()
+  .then(() => {
+    return nightmare
+      .goto('http://google.com')
+      .wait('#hplogo')
+      .evaluate(() => {
+        const x = document.getElementById('hplogo').getBoundingClientRect();
+        return {
+          y: x.top,
+          x: x.left,
+          width: x.width,
+          height: x.height
+        };
+      });
+  })
+  .then(rect => {
+    console.log(rect);
+    return nightmare.screenshot(undefined, rect);
+  })
+  .then((image) => {
+    console.log(image);
+    t.ok(true);
+
+    nightmare.end();
+    t.end();
+    count++;
+  });
+});
+
 setInterval(() => {
-  if (count === 2) {
+  if (count === 3) {
     process.exit();
   }
 }, 200);
