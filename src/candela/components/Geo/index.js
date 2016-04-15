28627162ae6fe15d52f13878922dc6f513005349
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2016-04-14 22:48:05 -0400

    Added Geojs plot

--- src/candela/components/Geo/index.js
+++ src/candela/components/Geo/index.js
@@ -1,9 +1,10 @@
 import spec from './spec.json';
 import $ from 'jquery';
-import jQuery from 'jquery';
 import geojs from 'geojs/geo.js';
+import VisComponent from '../../VisComponent';
+//import geojsext from 'geojs/geo.ext.min.js';
 
-export default class Geo {
+export default class Geo extends VisComponent {
   static get spec () {
     return {
       options: [
@@ -12,12 +13,14 @@ export default class Geo {
   }
 
   constructor (el, options) {
+    super(el);
     // this.chart = vega.chart(spec, el, options);
     this.plot = geojs.map({
                   node: el,
                   zoom: 6,
                   center: {x: 28.9550, y: 41.0136}
                 });
+    this.plot.createLayer('osm');
   }
 
   render () {
