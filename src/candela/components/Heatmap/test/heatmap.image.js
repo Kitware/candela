31587a9f1b52f3099b7234baaaacf773abe1e07c
Roni Choudhury <roni.choudhury@kitware.com>
2016-04-19 15:17:17 -0400

    Add heatmap image test

---
+++ src/candela/components/Heatmap/test/heatmap.image.js
@@ -0,0 +1,8 @@
+import imageTest from '../../../util/imageTest';
+
+imageTest({
+  name: 'heatmap',
+  url: 'http://localhost:38000/examples/#heatmap',
+  selector: '#vis-element',
+  threshold: 0.01
+});
