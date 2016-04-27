cd406b0eca8b92f913426940e7f709b978946bee
Roni Choudhury <roni.choudhury@kitware.com>
2016-04-27 12:52:10 -0400

    Abstract out structure tests into a function

--- src/candela/test/exports.js
+++ src/candela/test/exports.js
@@ -2,27 +2,30 @@ import test from 'tape';
 
 import candela from '..';
 
+function structureTests (t, cd) {
+  t.ok(cd, 'candela exists');
+
+  t.ok(cd.VisComponent, 'candela.VisComponent exists');
+
+  t.ok(cd.VisComponent.VisComponent, 'candela.VisComponent.VisComponent exists');
+  t.equal(typeof cd.VisComponent.VisComponent, 'function', 'candela.VisComponent.VisComponent is a function');
+
+  t.ok(cd.components, 'candela.components exists');
+
+  t.ok(cd.components.Histogram, 'candela.components.Histogram exists');
+  t.equal(typeof cd.components.Histogram, 'function', 'candela.components.Histogram is a function');
+
+  t.ok(cd.components.ParallelCoordinates, 'candela.components.ParallelCoordinates exists');
+  t.equal(typeof cd.components.ParallelCoordinates, 'function', 'candela.components.ParallelCoordinates is a function');
+
+  t.ok(cd.components.Scatter, 'candela.components.Scatter exists');
+  t.equal(typeof cd.components.Scatter, 'function', 'candela.components.Scatter is a function');
+
+  t.ok(cd.components.ScatterMatrix, 'candela.components.ScatterMatrix exists');
+  t.equal(typeof cd.components.ScatterMatrix, 'function', 'candela.components.ScatterMatrix is a function');
+}
+
 test('Structure and content of exported Candela library object', t => {
-  t.ok(candela, 'candela exists');
-
-  t.ok(candela.VisComponent, 'candela.VisComponent exists');
-
-  t.ok(candela.VisComponent.VisComponent, 'candela.VisComponent.VisComponent exists');
-  t.equal(typeof candela.VisComponent.VisComponent, 'function', 'candela.VisComponent.VisComponent is a function');
-
-  t.ok(candela.components, 'candela.components exists');
-
-  t.ok(candela.components.Histogram, 'candela.components.Histogram exists');
-  t.equal(typeof candela.components.Histogram, 'function', 'candela.components.Histogram is a function');
-
-  t.ok(candela.components.ParallelCoordinates, 'candela.components.ParallelCoordinates exists');
-  t.equal(typeof candela.components.ParallelCoordinates, 'function', 'candela.components.ParallelCoordinates is a function');
-
-  t.ok(candela.components.Scatter, 'candela.components.Scatter exists');
-  t.equal(typeof candela.components.Scatter, 'function', 'candela.components.Scatter is a function');
-
-  t.ok(candela.components.ScatterMatrix, 'candela.components.ScatterMatrix exists');
-  t.equal(typeof candela.components.ScatterMatrix, 'function', 'candela.components.ScatterMatrix is a function');
-
+  structureTests(t, candela);
   t.end();
 });
