b04415124eeaedd31fa19bd8b7256a36e66a34b1
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-05-03 11:38:50 -0400

    Fix claimProject error

--- app/resonant-laboratory/models/Project.js
+++ app/resonant-laboratory/models/Project.js
@@ -135,7 +135,7 @@ let Project = MetadataItem.extend({
     return this.create()
       .then(() => {
         // This copy is now "ours"
-        window.mainPage.currentUser.claimProject();
+        window.mainPage.currentUser.preferences.claimProject();
         // Let everyone know about the new project
         window.mainPage.trigger('rra:createProject');
         this.updateStatus();
