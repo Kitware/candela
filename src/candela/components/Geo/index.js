fa83406bdb6ec1a548eb219d7aa134062bbb9383
Roni Choudhury <roni.choudhury@kitware.com>
2016-05-10 13:52:20 -0400

    Fix linting errors

--- src/candela/components/Geo/index.js
+++ src/candela/components/Geo/index.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 import geojs from 'geojs/geo.js';
 import VisComponent from '../../VisComponent';
 
@@ -8,19 +7,21 @@ export default class Geo extends VisComponent {
     super(el);
     console.log(options);
     this.plot = geojs.map({
-                  node: el,
-                  zoom: 6,
-                  center: {x: 28.9550, y: 41.0136}
-                });
+      node: el,
+      zoom: 6,
+      center: {x: 28.9550, y: 41.0136}
+    });
     this.plot.createLayer('osm');
     if (options.features) {
-      for(let i = 0; i < options.features.length; ++i) {
+      for (let i = 0; i < options.features.length; ++i) {
         this.plot.createLayer('feature')
           .createFeature(options.features[i].type)
             .data(options.features[i].data)
-            .position(function(d) {
-              return {x: d[options.features[i].position.x],
-                      y: d[options.features[i].position.y]}
+            .position(function (d) {
+              return {
+                x: d[options.features[i].position.x],
+                y: d[options.features[i].position.y]
+              };
             });
       }
       this.plot.draw();
