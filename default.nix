e11920c52d9c176844150848afc2221be965f343
Roni Choudhury <roni.choudhury@kitware.com>
2016-05-23 11:23:47 -0400

    Enable image testing to work in nix-shell

--- default.nix
+++ default.nix
@@ -4,18 +4,45 @@
   name = "candela-build-env";
   targetPkgs = pkgs: (with pkgs;
     [ 
+      alsaLib
+      atk
       cairo
+      cups
+      dbus
+      expat
       fontconfig
       freetype
+      gdk_pixbuf
+      glib
       git
       gnumake
+      gtk
+      libnotify
       libpng
       nodejs
+      nspr
+      nss
+      pango
       pixman
       pkgconfig
       python
-      vim
+      udev
       zlib
+    ]) ++ (with pkgs.gnome;
+    [
+      GConf
+    ]) ++ (with pkgs.xorg;
+    [
+      libX11
+      libXcomposite
+      libXcursor
+      libXdamage
+      libXext
+      libXfixes
+      libXi
+      libXrandr
+      libXrender
+      libXtst
     ]);
   runScript = "zsh";
 }).env
