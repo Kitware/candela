5fa0f82baaf88915477e5b19a4d86145fa28c0fb
Roni Choudhury <roni.choudhury@kitware.com>
2016-06-03 15:17:47 -0400

    Add option spec (i.e., enable usage in Resonant Laboratory)

--- src/candela/components/GeoDots/index.js
+++ src/candela/components/GeoDots/index.js
@@ -2,6 +2,38 @@ import Geo from '../Geo';
 import VisComponent from '../../VisComponent';
 
 export default class GeoDots extends VisComponent {
+  static get spec () {
+    return {
+      options: [
+        {
+          name: 'data',
+          type: 'table',
+          format: 'objectlist'
+        },
+        {
+          name: 'x',
+          type: 'string',
+          format: 'text',
+          domain: {
+            mode: 'field',
+            from: 'data',
+            fieldTypes: ['number']
+          }
+        },
+        {
+          name: 'y',
+          type: 'string',
+          format: 'text',
+          domain: {
+            mode: 'field',
+            from: 'data',
+            fieldTypes: ['number']
+          }
+        }
+      ]
+    };
+  }
+
   constructor (el, options) {
     super(el);
 
