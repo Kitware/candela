4b000a18873fb6ce8e6e89276ce47535de324910
Roni Choudhury <roni.choudhury@kitware.com>
2016-06-08 08:16:40 -0400

    Add "baseLayer" option

--- src/candela/components/Geo/index.js
+++ src/candela/components/Geo/index.js
@@ -2,35 +2,37 @@ import geojs from 'geojs/geo.js';
 import VisComponent from '../../VisComponent';
 
 export default class Geo extends VisComponent {
-  constructor (el, options) {
+  constructor (el, {map = {}, baseLayer = {}, features = []}) {
     super(el);
 
-    const geojs_opt = options.map || {};
+    // Set default base layer options.
+    baseLayer.type = baseLayer.type || 'osm';
+    baseLayer.renderer = baseLayer.renderer || null;
 
+    // Construct a GeoJS map object based on the requested options.
     this.plot = geojs.map(Object.assign({
       node: el
-    }, geojs_opt));
+    }, map));
 
-    this.plot.createLayer('osm', {
-      renderer: null
+    // Create a base layer.
+    this.plot.createLayer(baseLayer.type, baseLayer);
+
+    // Set up requested feature layers.
+    features.forEach(feature => {
+      this.plot.createLayer('feature', {
+        renderer: 'd3'
+      })
+        .createFeature(feature.type)
+        .data(feature.data)
+        .position(d => ({
+          x: d[feature.x],
+          y: d[feature.y]
+        }))
+        .style('fillColor', 'red')
+        .style('strokeColor', 'darkred');
     });
 
-    if (options.features) {
-      options.features.forEach(feature => {
-        this.plot.createLayer('feature', {
-          renderer: 'd3'
-        })
-          .createFeature(feature.type)
-          .data(feature.data)
-          .position(d => ({
-            x: d[feature.x],
-            y: d[feature.y]
-          }))
-          .style('fillColor', 'red')
-          .style('strokeColor', 'darkred');
-      });
-      this.plot.draw();
-    }
+    this.render();
   }
 
   render () {
