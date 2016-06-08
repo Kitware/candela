f9ecb9b48646be528494fa636380e2198aca37f6
Roni Choudhury <roni.choudhury@kitware.com>
2016-06-08 08:22:18 -0400

    Allow baseLayer option to be null to avoid rendering a base layer at all

--- src/candela/components/Geo/index.js
+++ src/candela/components/Geo/index.js
@@ -5,17 +5,19 @@ export default class Geo extends VisComponent {
   constructor (el, {map = {}, baseLayer = {}, features = []}) {
     super(el);
 
-    // Set default base layer options.
-    baseLayer.type = baseLayer.type || 'osm';
-    baseLayer.renderer = baseLayer.renderer || null;
-
     // Construct a GeoJS map object based on the requested options.
     this.plot = geojs.map(Object.assign({
       node: el
     }, map));
 
     // Create a base layer.
-    this.plot.createLayer(baseLayer.type, baseLayer);
+    if (baseLayer !== null) {
+      // Set default base layer options.
+      baseLayer.type = baseLayer.type || 'osm';
+      baseLayer.renderer = baseLayer.renderer || null;
+
+      this.plot.createLayer(baseLayer.type, baseLayer);
+    }
 
     // Set up requested feature layers.
     features.forEach(feature => {
