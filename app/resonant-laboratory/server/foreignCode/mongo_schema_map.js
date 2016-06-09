8f0744c56b9b010a76b542b55eb814342d144ed4
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-06-09 11:45:35 -0400

    Count potential coercions in addition to native occurrences

--- app/resonant-laboratory/server/foreignCode/mongo_schema_map.js
+++ app/resonant-laboratory/server/foreignCode/mongo_schema_map.js
@@ -2,22 +2,96 @@
 function map () { // eslint-disable-line no-unused-vars
   var keys = this;
   for (var key in keys) {
+    // Create a dict containing counts for all possible data type interpretations
     var value = keys[key];
-    var dataType;
-    if (value === null) {
-      dataType = 'null';
-    } else {
-      dataType = typeof value;
+    var counters = {};
+
+    /** First determine and count the value's native type **/
+    var nativeType = typeof value;
+    if (nativeType === 'number') {
+      if (Math.floor(value) === value) {
+        nativeType = 'integer';
+      }
+    } else if (value instanceof Date) {
+      nativeType = 'date';
+    } else if (value === undefined) {
+      nativeType = 'undefined';
+    } else if (value === null) {
+      nativeType = 'null';
     }
 
-    var dataTypes = {};
-    dataTypes[dataType] = {
-      count: 1
+    // store the counts
+    counters[nativeType] = {
+      native: {
+        count: 1
+      }
     };
-    if (dataType === 'number') {
-      dataTypes[dataType].min = value;
-      dataTypes[dataType].max = value;
+    // store the mins and maxes if relevant
+    if (nativeType === 'integer' || nativeType === 'number' ||
+        nativeType === 'string' || nativeType === 'date') {
+      counters[nativeType].native.lowBound = value;
+      counters[nativeType].native.highBound = value;
     }
-    emit(key, dataTypes);
+
+    /** Next determine and count its potential coerced types **/
+    if (nativeType !== 'string') {
+      var strValue = String(value);
+      if (strValue !== '[object Object]') {
+        // this value can be meaningfully coerced to a string (and isn't already one)
+        counters['string'] = {
+          potential: {
+            count: 1,
+            lowBound: strValue,
+            highBound: strValue
+          }
+        };
+      }
+    }
+    if (nativeType !== 'number' && nativeType !== 'integer') {
+      var numValue = parseFloat(value);
+      if (!isNaN(numValue)) {
+        // this value can be coerced to a number (and isn't already one)
+        // is it an integer?
+        if (Math.floor(numValue) === numValue) {
+          counters['integer'] = {
+            potential: {
+              count: 1,
+              lowBound: numValue,
+              highBound: numValue
+            }
+          };
+        } else {
+          counters['number'] = {
+            potential: {
+              count: 1,
+              lowBound: numValue,
+              highBound: numValue
+            }
+          };
+        }
+      }
+    }
+    if (nativeType !== 'date') {
+      var dateValue = new Date(value);
+      if (!isNaN(dateValue)) {
+        // this value can be coerced into a date (and isn't already one)
+        counters['date'] = {
+          potential: {
+            count: 1,
+            lowBound: dateValue,
+            highBound: dateValue
+          }
+        };
+      }
+    }
+    if (nativeType !== 'boolean') {
+      // anything can be coerced into a boolean
+      counters['boolean'] = {
+        potential: {
+          count: 1
+        }
+      };
+    }
+    emit(key, counters);
   }
 }
--- app/resonant-laboratory/server/foreignCode/mongo_schema_reduce.js
+++ app/resonant-laboratory/server/foreignCode/mongo_schema_reduce.js
@@ -1,24 +1,43 @@
 function reduce (key, values) {  // eslint-disable-line no-unused-vars
-  var dataTypes = {};
-  values.forEach(function (value) {
-    var dataType;
-    for (dataType in value) {
-      if (!dataTypes.hasOwnProperty(dataType)) {
-        dataTypes[dataType] = {
-          count: 0
-        };
-        if (dataType === 'number') {
-          dataTypes[dataType].min = value[dataType].min;
-          dataTypes[dataType].max = value[dataType].max;
+  var counters = {};
+  values.forEach(function (counts) {
+    var dataType; // e.g. "integer", "string", ...
+    var countType;  // e.g. "native", "potential", ...
+    for (dataType in counts) {
+      if (counts.hasOwnProperty(dataType)) {
+        if (!counters.hasOwnProperty(dataType)) {
+          counters[dataType] = {};
         }
-      }
-      dataTypes[dataType].count += value[dataType].count;
-      if (dataType === 'number') {
-        dataTypes[dataType].min = value[dataType].min < dataTypes[dataType].min ? value[dataType].min : dataTypes[dataType].min;
-        dataTypes[dataType].max = value[dataType].max > dataTypes[dataType].max ? value[dataType].max : dataTypes[dataType].max;
+        for (countType in counts[dataType]) {
+          if (counts[dataType].hasOwnProperty(countType)) {
+            if (!counters[dataType].hasOwnProperty(countType)) {
+              // We haven't seen this count yet; initialize it with
+              // a zero count, and low/high bounds if relevant
+              counters[dataType][countType] = {
+                count: 0
+              };
+              if (dataType === 'integer' || dataType === 'number' ||
+                  dataType === 'string' || dataType === 'date') {
+                counters[dataType][countType].lowBound = counts[dataType][countType].lowBound;
+                counters[dataType][countType].highBound = counts[dataType][countType].highBound;
+              }
+            }
+            // Add the count, and update the low/high bounds
+            counters[dataType][countType].count += counts[dataType][countType].count;
+            if (dataType === 'integer' || dataType === 'number' ||
+                dataType === 'string' || dataType === 'date') {
+              if (counts[dataType][countType].lowBound < counters[dataType][countType].lowBound) {
+                counters[dataType][countType].lowBound = counts[dataType][countType].lowBound;
+              }
+              if (counts[dataType][countType].highBound > counters[dataType][countType].highBound) {
+                counters[dataType][countType].highBound = counts[dataType][countType].highBound;
+              }
+            }
+          }
+        }
       }
     }
   });
 
-  return dataTypes;
+  return counters;
 }
