fcfbf2f921daca686d37ca3054df480360eeea5e
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-06-13 15:14:58 -0400

    Support automatic creation of mongo collections in place of flat file uploads

--- app/resonant-laboratory/examples/populateGirder.py
+++ app/resonant-laboratory/examples/populateGirder.py
@@ -5,150 +5,205 @@ import girder_client
 import os
 import json
 import sys
-
-parser = argparse.ArgumentParser(description='''Populate a girder
-    installation with example dataset and project collections.
-    This script is idempotent, so running it multiple times won't
-    result in duplicate files (though it may revert any changes
-    that have been made to the elements that it originally added)''')
-parser.add_argument('-c, --clean', dest='clean', action='store_true',
-                    help='Remove the ResonantLaboratory collection before re-populating')
-parser.add_argument('-u, --username', dest='username', default='admin',
-                    help='The administrator username (default: "admin")')
-parser.add_argument('-a, --apiUrl', dest='apiUrl',
-                    default='http://localhost:8080/api/v1',
-                    help='''The url of the Girder instance's API endpoint.''')
-
-args = parser.parse_args()
-password = getpass.getpass()
-
-gc = girder_client.GirderClient(apiUrl=args.apiUrl)
-gc.authenticate(args.username, password)
-
-message = ''
-
-# Get or create the ResonantLaboratory collection
-collection = gc.sendRestRequest('GET', 'collection',
-                                {'text': 'ResonantLaboratory'})
-
-# If specified, trash the existing ResonantLaboratory collection
-if (args.clean):
-    if (len(collection) == 0):
-        print 'No "ResonantLaboratory" collection to clean.'
-        print
-    else:
-        gc.sendRestRequest('DELETE', 'collection/' + collection[0]['_id'])
-        print 'Deleted "ResonantLaboratory" collection.'
-        print
-        collection = []
-
-# Create a new collection if needed
-if len(collection) == 0:
-    collection = gc.sendRestRequest('POST', 'collection',
-                                    {'name': 'ResonantLaboratory',
-                                     'description': 'The public library for' +
-                                                    ' the Resonant ' +
-                                                    'Laboratory Application',
-                                     'public': True})
-    message += 'Created collection '
-else:
-    collection = collection[0]
-    message += 'Using existing collection '
-collectionID = collection['_id']
-
-message += collectionID
-print message
-print ''.join(['=' for x in message])   # underline
+import subprocess
 
 
-# Create the Data and Projects folders
-dataItemIdLookup = {}
-for f in ['Data', 'Projects']:
-    if not os.path.isdir(f):
-        continue
+def getArguments():
+    parser = argparse.ArgumentParser(description='''Populate a girder installation
+        with the public library (datasets and projects) for Resonant Laboratory.
+        This script is idempotent, so running it multiple times won't
+        result in duplicate files (though it may revert any changes
+        that have been made to the elements that it originally added)''')
+    parser.add_argument('-u', dest='username', default='admin',
+                        help='The administrator username (default: "admin")')
+    parser.add_argument('-a', dest='apiUrl',
+                        default='http://localhost:8080/api/v1',
+                        help='The url of the Girder instance\'s API endpoint.')
+    parser.add_argument('-c', dest='clean', action='store_true',
+                        help='Remove the ResonantLaboratory collection before re-populating')
+    parser.add_argument('-d', dest='databaseThreshold', default='131072',
+                        help='If a file exceeds this threshold (in bytes), upload it as a ' +
+                        'mongo database collection instead of a flat file.')
+    parser.add_argument('-m', dest='mongoHost',
+                        default='localhost',
+                        help='The server hosting the mongo instance in which to store larger files.')
+    parser.add_argument('-p', dest='mongoPort',
+                        default='27017',
+                        help='The port of the mongo instance in which to store larger files.')
+    parser.add_argument('-n', dest='dbName',
+                        default='resonantLaboratoryLibrary',
+                        help='The name of the mongo db to use for larger files.')
 
-    print
-    print '## Folder: ' + f
-    print
+    return parser.parse_args()
 
-    folderSpec = gc.load_or_create_folder(f, collectionID, 'collection')
 
-    # The second-level directories correspond to items
-    items = os.listdir('./' + f)
-    if len(items) == 0:
-        continue
+def getGirderCollection(args):
+    print 'Enter the password for Girder user "' + args.username + '":'
+    password = getpass.getpass()
 
-    longestItemName = max([len(i) for i in items])
+    gc = girder_client.GirderClient(apiUrl=args.apiUrl)
+    gc.authenticate(args.username, password)
 
-    for i in items:
-        if not os.path.isdir('./' + f + '/' + i):
-            continue
-        print '- Item: ' + i,
+    message = ''
 
-        spacesNeeded = longestItemName - len(i)
-        message = ''.join([' ' for x in xrange(spacesNeeded)])
+    # Get or create the ResonantLaboratory collection
+    collection = gc.sendRestRequest('GET', 'collection',
+                                    {'text': 'ResonantLaboratory'})
 
-        # Create (or get) the item
-        itemSpec = gc.load_or_create_item(i, folderSpec['_id'])
-
-        # If this is a dataset, store its ID for Projects
-        # to look up later
-        if f == 'Data':
-            dataItemIdLookup[i] = itemSpec['_id']
-
-        # Now upload any files that don't already
-        # exist in the item
-        files = os.listdir('./' + f + '/' + i)
-
-        existingFiles = gc.sendRestRequest('GET', 'item/' + itemSpec['_id'] +
-                                           '/files', {})
-        existingFiles = set([x['name'] for x in existingFiles])
-
-        ignoredFiles = 0
-        addedFiles = 0
-        addedMetadata = False
-
-        # Load each of the files into the item
-        for x in files:
-            if x == 'metadata.json':
-                # metadata.json is special; attach it as the item's
-                # metadata instead of uploading it as a file
-                temp = open('./' + f + '/' + i + '/metadata.json', 'rb')
-                contents = temp.read()
-                metadata = json.loads(contents)
-                temp.close()
-
-                # If this is a project, we need to replace the dataset
-                # folder name with a Girder ID
-                if f == 'Projects':
-                    for i, d in enumerate(metadata['datasets']):
-                        metadata['datasets'][i]['itemId'] = dataItemIdLookup[d['itemId']]
-
-                gc.addMetadataToItem(itemSpec['_id'], metadata)
-                addedMetadata = True
-            elif x in existingFiles:
-                ignoredFiles += 1
-            else:
-                fileObj = gc.uploadFileToItem(itemSpec['_id'], './' + f + '/' +
-                                              i + '/' + x)
-                addedFiles += 1
-
-        # Hit the endpoint that identifies the item as a dataset or a project
-        if f == 'Data':
-            gc.sendRestRequest('POST', 'item/' + itemSpec['_id'] + '/dataset')
-        elif f == 'Projects':
-            # TODO
-            pass
-
-        message += '%i file%s loaded' % (addedFiles,
-                                         '' if addedFiles == 1 else 's')
-        message += '\t%i file%s ignored' % (ignoredFiles,
-                                            '' if ignoredFiles == 1 else 's')
-        if addedMetadata is False:
-            message += '\tCOULD NOT FIND METADATA'
+    # If specified, trash the existing ResonantLaboratory collection
+    if (args.clean):
+        if (len(collection) == 0):
+            print 'No "ResonantLaboratory" collection to clean.'
+            print
         else:
-            message += '\tmetadata attached'
-        print message
+            gc.sendRestRequest('DELETE', 'collection/' + collection[0]['_id'])
+            print 'Deleted "ResonantLaboratory" collection.'
+            print
+            collection = []
 
-print 'finished'
-print
+    # Create a new collection if needed
+    if len(collection) == 0:
+        collection = gc.sendRestRequest('POST', 'collection',
+                                        {'name': 'ResonantLaboratory',
+                                         'description': 'The public library for' +
+                                                        ' the Resonant ' +
+                                                        'Laboratory Application',
+                                         'public': True})
+        message += 'Created collection '
+    else:
+        collection = collection[0]
+        message += 'Using existing collection '
+    collectionID = collection['_id']
+
+    message += collectionID
+    print message
+    print ''.join(['=' for x in message])   # underline
+
+    return (gc, collectionID)
+
+
+def createMongoCollection(args, filePath, fileName):
+    # TODO: do this with pymongo, not mongo-import
+    parts = os.path.splitext(fileName)
+    command = ['mongoimport',
+               '--host', args.mongoHost,
+               '--port', args.mongoPort,
+               '--db', args.dbName,
+               '--collection', fileName[0],
+               '--drop',
+               '--file', os.path.join(filePath, fileName)]
+    if parts[1].lower() == '.csv':
+        command.extend(['--type', 'csv',
+                        '--headerline'])
+    else:
+        command.append('--jsonArray')
+    subprocess.check_output(command, stderr=subprocess.STDOUT)
+
+
+if __name__ == '__main__':
+    args = getArguments()
+    gc, collectionID = getGirderCollection(args)
+
+    # Create the Data and Projects folders
+    dataItemIdLookup = {}
+    for folder in ['Data', 'Projects']:
+        if not os.path.isdir(folder):
+            continue
+
+        print
+        print '## ' + folder + ':'
+        print ''
+
+        folderSpec = gc.load_or_create_folder(folder, collectionID, 'collection')
+
+        # The second-level directories correspond to items
+        items = os.listdir('./' + folder)
+        if len(items) == 0:
+            continue
+
+        longestItemName = max([len(item) for item in items])
+
+        print 'Item' + ''.join([' ' for x in xrange(longestItemName - 4)]),
+        print '\tIgnored files\tFlat files added\tCollections added\tMetadata attached'
+
+        for item in items:
+            if not os.path.isdir('./' + folder + '/' + item):
+                continue
+            print item,
+
+            spacesNeeded = longestItemName - len(item)
+            message = ''.join([' ' for x in xrange(spacesNeeded)])
+
+            # Create (or get) the item
+            itemSpec = gc.load_or_create_item(item, folderSpec['_id'])
+
+            # If this is a dataset, store its ID for Projects
+            # to look up later
+            if folder == 'Data':
+                dataItemIdLookup[item] = itemSpec['_id']
+
+            # Now upload any files that don't already
+            # exist in the item
+            files = os.listdir('./' + folder + '/' + item)
+
+            existingFiles = gc.sendRestRequest('GET', 'item/' + itemSpec['_id'] + '/files', {})
+            existingFiles = set([x['name'] for x in existingFiles])
+
+            ignoredFiles = 0
+            addedFiles = 0
+            addedCollections = 0
+            addedMetadata = False
+
+            # Load each of the files into the item
+            for fileName in files:
+                if fileName == 'metadata.json':
+                    # metadata.json is special; attach it as the item's
+                    # metadata instead of uploading it as a file
+                    temp = open('./' + folder + '/' + item + '/metadata.json', 'rb')
+                    contents = temp.read()
+                    metadata = json.loads(contents)
+                    temp.close()
+
+                    # If this is a project, we need to replace the dataset
+                    # folder name with a Girder ID
+                    if folder == 'Projects':
+                        for i, d in enumerate(metadata['datasets']):
+                            metadata['datasets'][i]['itemId'] = dataItemIdLookup[d['itemId']]
+
+                    gc.addMetadataToItem(itemSpec['_id'], metadata)
+                    addedMetadata = True
+                elif fileName in existingFiles:
+                    ignoredFiles += 1
+                else:
+                    fileSize = os.stat('./' + folder + '/' + item + '/' + fileName).st_size
+                    if (fileSize > int(args.databaseThreshold)):
+                        createMongoCollection(args, './' + folder + '/' + item + '/', fileName)
+                        gc.sendRestRequest('POST', 'item/' + itemSpec['_id'] + '/database', {}, json.dumps({
+                            'url': args.mongoHost + ':' + args.mongoPort,
+                            'database': args.dbName,
+                            'collection': os.path.splitext(fileName)[0],
+                            'type': 'mongo'
+                        }))
+                        addedCollections += 1
+                    else:
+                        gc.uploadFileToItem(itemSpec['_id'], './' + folder + '/' + item + '/' + fileName)
+                        addedFiles += 1
+
+            # Hit the endpoint that identifies the item as a dataset or a project,
+            # and populates the metadata appropriately
+            if folder == 'Data':
+                gc.sendRestRequest('POST', 'item/' + itemSpec['_id'] + '/dataset')
+            elif folder == 'Projects':
+                # TODO
+                pass
+
+            message += '\t%i            \t%i               \t%i                \t' % (
+                ignoredFiles, addedFiles, addedCollections)
+            if addedMetadata is False:
+                message += 'N'
+            else:
+                message += 'Y'
+            print message
+
+    print 'finished'
+    print
