9e332391a5610d6528c6d44482902f9731fe52d8
Roni Choudhury <roni.choudhury@kitware.com>
2016-07-12 14:54:55 -0400

    Factor out test cases into common function

--- app/resonant-laboratory/server/test-querylang.py
+++ app/resonant-laboratory/server/test-querylang.py
@@ -15,41 +15,31 @@ data = [
 
 class TestQueryLanguage(unittest.TestCase):
     """Test suite."""
+    def run_expressions(self, ast_file, data, baseline):
+        """Test several ASTs on a sequence of data against a set of baselines."""
+        asts = None
+        with open(ast_file) as f:
+            asts = json.load(f)
+
+        funcs = map(ql.astToFunction, asts)
+
+        for ((f, a), d), b in zip(itertools.product(zip(funcs, asts), data), baseline):
+            self.assertEqual(f(d), b, '%s on %s should be %s' % (a, d, not b))
 
     def test_operator_expressions(self):
         """Test operator expressions."""
-        asts = None
-        with open('test/operator-ast-baselines.json') as f:
-            asts = json.load(f)
-
-        baseline = [
+        self.run_expressions('test/operator-ast-baselines.json', data, [
             True, False, False,
             True, True, False,
             False, False, True,
             False, True, True,
             False, True, False,
             True, False, True
-        ]
-
-        funcs = map(ql.astToFunction, asts)
-
-        for ((f, a), d), b in zip(itertools.product(zip(funcs, asts), data), baseline):
-            self.assertEqual(f(d), b, '%s on %s should be %s' % (a, d, not b))
+        ])
 
     def test_conjunction_expressions(self):
         """Test conjunction expressions."""
-        asts = None
-        with open('test/conjunction-ast-baselines.json') as f:
-            asts = json.load(f)
-
-        baseline = [
-            False, True, False
-        ]
-
-        funcs = map(ql.astToFunction, asts)
-
-        for ((f, a), d), b in zip(itertools.product(zip(funcs, asts), data), baseline):
-            self.assertEqual(f(d), b, '%s on %s should be %s' % (a, d, not b))
+        self.run_expressions('test/conjunction-ast-baselines.json', data, [False, True, False])
 
 
 if __name__ == '__main__':
