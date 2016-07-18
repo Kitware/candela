28d1d1fd7bf70597bc068bc80502cd5c57de501b
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-07-18 16:01:03 -0400

    Add more tests

--- app/resonant-laboratory/shims/test/rangeSet.js
+++ app/resonant-laboratory/shims/test/rangeSet.js
@@ -5,6 +5,37 @@ function stringComparator (a, b) {
   return a.localeCompare(b);
 }
 
+let unboundedRange = {};
+let invalidRange = {
+  lowBound: 10,
+  highBound: 0
+};
+let regularRange = {
+  lowBound: 4,
+  highBound: 8
+};
+let highBoundedRange = {
+  highBound: 7
+};
+let lowBoundedRange = {
+  lowBound: 5
+};
+
+let testRanges = [
+  [unboundedRange, regularRange],
+  [highBoundedRange, lowBoundedRange],
+  [invalidRange],
+  [highBoundedRange, regularRange],
+  [lowBoundedRange, regularRange]
+];
+let testRanges2 = [
+  [invalidRange],
+  [highBoundedRange],
+  [lowBoundedRange],
+  [regularRange],
+  [regularRange]
+];
+
 test('RangeSet: Finding most extreme values', t => {
   let range = [1, 3, -5, 2];
   t.equal(RangeSet.mostExtremeValue(range, '>'), 3, `3 is the highest value in [${range}]`);
@@ -19,48 +50,84 @@ test('RangeSet: Finding most extreme values', t => {
 });
 
 test('RangeSet: Cleaning range lists', t => {
-  let unboundedRange = {};
-  let invalidRange = {
-    lowBound: 10,
-    highBound: 0
-  };
-  let regularRange = {
-    lowBound: 4,
-    highBound: 8
-  };
-  let highBoundedRange = {
-    highBound: 7
-  };
-  let lowBoundedRange = {
-    lowBound: 5
-  };
-
-  let testRanges = [
-    [unboundedRange, regularRange],
-    [highBoundedRange, lowBoundedRange],
-    [invalidRange],
-    [highBoundedRange, regularRange],
-    [lowBoundedRange, regularRange]
-  ];
-  let testSolutions = [
+  let cleaningSolutions = [
     [unboundedRange],
     [unboundedRange],
     [],
     [{
-      highBound: 7,
-      lowBound: 4
+      highBound: 8
     }],
     [{
-      highBound: 8,
-      lowBound: 5
+      lowBound: 4
     }]
   ];
-
   for (let i = 0; i < testRanges.length; i += 1) {
     let outputRange = JSON.stringify(testRanges[i]);
-    let outputSolution = JSON.stringify(testSolutions[i]);
-    t.deepEqual(RangeSet.cleanRangeList(testRanges[i]), testSolutions[i],
+    let outputSolution = JSON.stringify(cleaningSolutions[i]);
+    t.deepEqual(RangeSet.cleanRangeList(testRanges[i]), cleaningSolutions[i],
       `Clean ${outputRange} to become ${outputSolution}`);
+  }
+  t.end();
+});
+
+test('RangeSet: Union', t => {
+  let unionSolutions = [
+    [unboundedRange],
+    [unboundedRange],
+    [lowBoundedRange],
+    [{
+      highBound: 8
+    }],
+    [{
+      lowBound: 4
+    }]
+  ];
+  for (let i = 0; i < testRanges.length; i += 1) {
+    let outputRange = JSON.stringify(testRanges[i]);
+    let outputRange2 = JSON.stringify(testRanges2[i]);
+    let outputSolution = JSON.stringify(unionSolutions[i]);
+    t.deepEqual(RangeSet.rangeUnion(testRanges[i], testRanges2[i]), unionSolutions[i],
+      `${outputRange} union ${outputRange2} = ${outputSolution}`);
+  }
+  t.end();
+});
+
+test('RangeSet: Intersection', t => {
+  let unionSolutions = [
+    [],
+    [highBoundedRange],
+    [],
+    [regularRange],
+    [regularRange]
+  ];
+  for (let i = 0; i < testRanges.length; i += 1) {
+    let outputRange = JSON.stringify(testRanges[i]);
+    let outputRange2 = JSON.stringify(testRanges2[i]);
+    let outputSolution = JSON.stringify(unionSolutions[i]);
+    t.deepEqual(RangeSet.rangeIntersection(testRanges[i], testRanges2[i]), unionSolutions[i],
+      `${outputRange} union ${outputRange2} = ${outputSolution}`);
+  }
+  t.end();
+});
+
+test('RangeSet: Subtraction', t => {
+  let unionSolutions = [
+    [unboundedRange],
+    [],
+    [],
+    [{
+      highBound: 4
+    }],
+    [{
+      lowBound: 8
+    }]
+  ];
+  for (let i = 0; i < testRanges.length; i += 1) {
+    let outputRange = JSON.stringify(testRanges[i]);
+    let outputRange2 = JSON.stringify(testRanges2[i]);
+    let outputSolution = JSON.stringify(unionSolutions[i]);
+    t.deepEqual(RangeSet.rangeSubtract(testRanges[i], testRanges2[i]), unionSolutions[i],
+      `${outputRange} union ${outputRange2} = ${outputSolution}`);
   }
   t.end();
 });
