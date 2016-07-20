6c477ba363c0d9b4ff8c4afa541211b194cb844b
Roni Choudhury <roni.choudhury@kitware.com>
2016-07-20 11:00:52 -0400

    Enable faceting on "treatment type" etc.

--- app/examples/survival/index.js
+++ app/examples/survival/index.js
@@ -5,31 +5,38 @@ window.onload = () => {
     data: [
       {
         time: 4,
+        drug: 'A',
         censor: 1
       },
       {
         time: 16,
+        drug: 'B',
         censor: 0
       },
       {
         time: 8,
+        drug: 'A',
         censor: 0
       },
       {
         time: 42,
+        drug: 'B',
         censor: 1
       },
       {
         time: 23,
+        drug: 'A',
         censor: 0
       },
       {
         time: 15,
+        drug: 'B',
         censor: 1
       }
     ],
     time: 'time',
     censor: 'censor',
+    group: 'drug',
     width: 625,
     height: 540,
     padding: {
--- src/candela/components/SurvivalPlot/index.js
+++ src/candela/components/SurvivalPlot/index.js
@@ -29,6 +29,17 @@ export default class SurvivalPlot extends VegaChart(VisComponent, spec) {
           from: 'data',
           fieldTypes: ['integer', 'boolean']
         }
+      },
+      {
+        name: 'group',
+        type: 'string',
+        format: 'text',
+        optional: true,
+        domain: {
+          mode: 'field',
+          from: 'data',
+          fieldTypes: ['string', 'date', 'number', 'integer', 'boolean']
+        }
       }
     ];
   }
@@ -39,18 +50,45 @@ export default class SurvivalPlot extends VegaChart(VisComponent, spec) {
     // Sort the events by time.
     options.data.sort((x, y) => x[options.time] - y[options.time]);
 
-    // Append a dummy value to the start of the dataset.
-    options.data = [{time: 0}, ...options.data];
-    let surv = options.data.length;
+    // If there is no 'group' parameter, fake one up.
+    if (!options.group) {
+      options.group = 'group';
+      options.data.forEach(d => d.group = 0);
+    }
+
+    // Collect the grouping values, and count how many belong to each group.
+    let groups = {};
+    options.data.forEach(d => {
+      const key = d[options.group];
+      if (!groups[key]) {
+        groups[key] = {
+          survivors: 1
+        };
+      }
+
+      groups[key].survivors++;
+    });
+
+    // Append a dummy value to the start of the dataset, one per group.
+    const dummies = Object.keys(groups).map(g => {
+      let x = {
+        time: 0
+      };
+
+      x[options.group] = g;
+
+      return x;
+    });
+    options.data = [...dummies, ...options.data];
 
     // Compute the number of survivors at each event. A patient is assumed to
     // have not survived, unless the reason for the event is "right censoring"
     // (aka, censor == 1).
     options.data.forEach(d => {
       if (d[options.censor] !== 0) {
-        surv--;
+        groups[d[options.group]].survivors--;
       }
-      d.survivors = surv;
+      d.survivors = groups[d[options.group]].survivors;
     });
   }
 }
