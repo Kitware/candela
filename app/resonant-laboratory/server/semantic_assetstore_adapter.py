7fe1b901c61d56225bc74262cdae04f4423e17ff
Roni Choudhury <roni.choudhury@kitware.com>
2016-07-26 11:09:31 -0400

    Make db assetstore return csv so that semantic assetstore works for all item types

--- app/resonant-laboratory/server/semantic_assetstore_adapter.py
+++ app/resonant-laboratory/server/semantic_assetstore_adapter.py
@@ -106,20 +106,24 @@ class StreamFile(object):
         pass
 
 
-def semantic_access(Cls):
-    allowed_filetypes = ['csv']
+def semantic_access(Cls, offset_limit=True):
     allowed_outputtypes = ['csv', 'json', 'jsonArray']
 
     module = 'resonant-laboratory.semantic-filesystem-assetstore-adapter'
 
     class NewCls(Cls):
         def __init__(self, *args, **kwargs):
+            self.offset_limit = offset_limit
             super(NewCls, self).__init__(*args, **kwargs)
 
         def downloadFile(self, file, offset=0, headers=True, endByte=None,
                          contentDisposition=None, extraParameters=None, **kwargs):
+            if extraParameters is not None:
+                extraParameters = json.loads(extraParameters)
+                extraParameters['format'] = 'csv'
+
             # Get the parent class's stream.
-            base_stream = super(NewCls, self).downloadFile(file, offset, headers, endByte, contentDisposition, extraParameters, **kwargs)
+            base_stream = super(NewCls, self).downloadFile(file, offset, headers, endByte, contentDisposition, json.dumps(extraParameters), **kwargs)
 
             # Fall back to base class when no special parameters are specified.
             if extraParameters is None:
@@ -128,18 +132,11 @@ def semantic_access(Cls):
             # Construct and return our own stream that implements the special
             # behaviors requested in extraParameters on top of the base class's
             # stream.
-            extraParameters = json.loads(extraParameters)
-            offset = extraParameters.get('offset', 0)
-            limit = extraParameters.get('limit', 0)
-
-            fileType = extraParameters.get('fileType')
-            if fileType is None:
-                print 'fileType = None'
-                raise GirderException('"fileType" argument is required', '%s.missing-required-argument' % (module))
-
-            if fileType not in allowed_filetypes:
-                print 'fileType = %s is not allowed' % (fileType)
-                raise GirderException('"fileType" must be one of: %s' % (', '.join(allowed_filetypes)), '%s.illegal-argument' % (module))
+            if self.offset_limit:
+                offset = extraParameters.get('offset', 0)
+                limit = extraParameters.get('limit', 0)
+            else:
+                offset = limit = 0
 
             outputType = extraParameters.get('outputType')
             if outputType is None:
