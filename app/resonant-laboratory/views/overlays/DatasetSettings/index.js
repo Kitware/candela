a039b598e489f0044e7197289036f50ea1e6b96b
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-08-03 13:52:39 -0400

    Create initial layout mockup

--- app/resonant-laboratory/views/overlays/DatasetSettings/index.js
+++ app/resonant-laboratory/views/overlays/DatasetSettings/index.js
@@ -1,18 +1,29 @@
-import Backbone from 'backbone';
+import SettingsPanel from '../SettingsPanel';
 // import d3 from 'd3';
 import myTemplate from './template.html';
 
-let DatasetSettings = Backbone.View.extend({
+let DatasetSettings = SettingsPanel.extend({
   initialize: function () {
+    SettingsPanel.prototype.initialize.apply(this, [
+      'Dataset Settings',
+      'blurb',
+      [
+        {
+          text: 'Save a copy',
+          onclick: () => null
+        }
+      ]
+    ]);
     this.listenTo(window.mainPage.project, 'rl:changeDatasets', () => {
       this.render();
     });
   },
   render: function () {
-    let self = this;
+    // First the main container
     if (!this.addedTemplate) {
-      this.$el.html(myTemplate);
-      this.addedTemplate = true;
+      // SettingsPnale.render will set this.addedTemplate to true
+      SettingsPanel.prototype.render.apply(this, arguments);
+      this.$el.find('#subclassContent').html(myTemplate);
     }
 
     // Update the paging parameters
@@ -20,13 +31,13 @@ let DatasetSettings = Backbone.View.extend({
     let limitInput = this.$el.find('#limitInput');
     limitInput.val(datasetObj.cache.page.limit);
     this.$el.find('#limitButton').on('click', () => {
-      self.$el.find('#totalPages').text('...');
+      this.$el.find('#totalPages').text('...');
       datasetObj.setLimit(parseInt(limitInput.val(), 10));
     });
     datasetObj.cache.filteredHistogram.then(histogram => {
       let count = Math.min(histogram.__passedFilters__[0].count,
         datasetObj.cache.page.limit);
-      self.$el.find('#totalPages').text(count);
+      this.$el.find('#totalPages').text(count);
     });
   }
 });
---
+++ app/resonant-laboratory/views/overlays/SettingsPanel/index.js
@@ -0,0 +1,47 @@
+import d3 from 'd3';
+import Underscore from 'underscore';
+import Backbone from 'backbone';
+import template from './template.html';
+import './style.scss';
+
+let SettingsPanel = Backbone.View.extend({
+  initialize: function (title, blurb, actionItems) {
+    this.title = title;
+    this.blurb = blurb;
+    this.actionItems = actionItems;
+  },
+  render: function () {
+    if (!this.addedTemplate) {
+      let options = {
+        title: this.title,
+        blurb: this.blurb || ''
+      };
+      this.$el.html(Underscore.template(template)(options));
+      this.addedTemplate = true;
+    }
+
+    this.$el.find('a#loginLink').on('click', () => {
+      window.mainPage.overlay.render('LoginView');
+    });
+
+    this.$el.find('a#registerLink').on('click', () => {
+      window.mainPage.overlay.render('RegisterView');
+    });
+
+    if (window.mainPage.currentUser.isLoggedIn()) {
+      this.$el.find('#loginLinks').hide();
+    } else {
+      this.$el.find('#loginLinks').show();
+    }
+
+    let actionItems = d3.select('#actionItems')
+      .selectAll('div.actionItem').data(this.actionItems);
+    actionItems.enter().append('div')
+      .attr('class', 'actionItem');
+    actionItems.exit().remove();
+    actionItems.text(d => d.text)
+      .on('click', d => d.onclick);
+  }
+});
+
+export default SettingsPanel;
