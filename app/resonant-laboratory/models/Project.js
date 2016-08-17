451a1c210edf8e82012e636a51ce4ab249b497bb
Alex Bigelow <alex.r.bigelow@gmail.com>
2016-08-17 11:37:06 -0400

    Only use fresh metadata to validate matchings

--- app/resonant-laboratory/models/Project.js
+++ app/resonant-laboratory/models/Project.js
@@ -446,7 +446,6 @@ let Project = MetadataItem.extend({
     });
   },
   checkAndSaveMatchings: function (triggers) {
-    let meta = this.getMeta();
     triggers = triggers || [];
 
     // Go through all the matchings and make sure that:
@@ -458,6 +457,7 @@ let Project = MetadataItem.extend({
     return this.save().then(() => {
       return this.cache.loadedDatasets;
     }).then(loadedDatasets => {
+      let meta = this.getMeta();
       let indicesToTrash = [];
       for (let [index, matching] of meta.matchings.entries()) {
         if (meta.datasets.length <= matching.dataIndex ||
