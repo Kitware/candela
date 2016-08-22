fae2cd2323d5df54cddedfcee47548ca8d017d7d
Jeffrey Baumes <jeff.baumes@kitware.com>
2016-08-22 12:48:05 -0400

    Adding setuptools files

---
+++ python/pycandela/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='pycandela',
+    version='0.2.0',
+    description='Jupyter interface for Candela visualization components',
+    author='Kitware, Inc.',
+    author_email='kitware@kitware.com',
+    license='Apache 2.0',
+    url='https://github.com/Kitware/candela',
+    packages=['pycandela'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+    install_requires=[
+        'jupyter',
+        'pandas'
+    ]
+)
