c9a1060512bdebafe42f3a4b2dd5c13f39f1a9c0
Jeffrey Baumes <jeff.baumes@kitware.com>
2016-08-30 13:52:41 -0400

    Update pycandela api to be more pythonic, adding flake8 test

--- python/pycandela/pycandela/__init__.py
+++ python/pycandela/pycandela/__init__.py
@@ -1,35 +1,16 @@
-import IPython.core.displaypub as displaypub
+from functools import partial
+from IPython.display import display, publish_display_data
 import json
 from pandas import DataFrame
 
-class DataFrameEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, DataFrame):
-            return obj.to_dict(orient='records')
-        return json.JSONEncoder.default(self, obj)
+try:
+    __IPYTHON__
+except NameError:
+    raise ImportError('pycandela must be imported from IPython')
 
-def publish_display_data(data):
-    try:
-        displaypub.publish_display_data('pycandela', data)
-    except TypeError:
-        displaypub.publish_display_data(data)
+_component_list = []
 
-
-def component(name, options):
-    js = ("""
-(function (el) {
-    require(['candela'], function (candela) {
-        var vis = new candela.components['%s'](el.get(0), %s);
-        vis.render();
-    });
-})(element);
-""" % (name, json.dumps(options, cls=DataFrameEncoder)))
-
-    publish_display_data({'application/javascript': js})
-
-
-def init():
-    js = """
+js = """
 require.config({
     paths: {
         candela: 'http://kitware.github.io/candela/candela-0.2.0-81be44f6'
@@ -38,13 +19,62 @@ require.config({
 
 (function (el) {
     require(['candela'], function (candela) {
-        if (candela) {
-            el.append('<div>Candela loaded successfully.</div>');
-        } else {
-            el.append('<div>Error loading Candela.</div>');
+        var components = [];
+        for (var comp in candela.components) {
+            if (candela.components.hasOwnProperty(comp)) {
+                components.push(comp);
+            }
         }
+        var kernel = IPython.notebook.kernel;
+        kernel.execute('import pycandela as __pycandela');
+        kernel.execute('__pycandela._component_list = ' +
+            JSON.stringify(components));
+    }, function (error) {
+        el.append('<pre>' + error + '</pre>');
     });
 })(element);
 """
 
-    publish_display_data({'application/javascript': js})
+publish_display_data({'application/javascript': js})
+
+
+class DataFrameEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, DataFrame):
+            return obj.to_dict(orient='records')
+        return json.JSONEncoder.default(self, obj)
+
+
+class Component(object):
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.options = kwargs
+
+    def _ipython_display_(self):
+        js = ("""
+            (function (el) {
+                require(['candela'], function (candela) {
+                    var comp = candela.components['%s'];
+                    var vis = new comp(el.get(0), %s);
+                    vis.render();
+                });
+            })(element);
+            """ % (self.name, json.dumps(self.options, cls=DataFrameEncoder)))
+        publish_display_data({'application/javascript': js})
+
+    def display(self):
+        display(self)
+
+
+class ComponentAccessor(object):
+    def __dir__(self):
+        return _component_list
+
+    def __getattr__(self, name):
+        if name not in _component_list:
+            raise AttributeError(
+                "'" + type(self).__name__ +
+                "' class has no attribute '" + name + "'")
+        return partial(Component, name)
+
+components = ComponentAccessor()
