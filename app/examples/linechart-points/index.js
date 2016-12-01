c814752dd189c7641d7bad18055bcab5311aa3b7
Roni Choudhury <roni.choudhury@kitware.com>
2016-11-30 19:14:15 -0500

    test(LineChart): add linechart-with-points example and test

---
+++ app/examples/linechart-points/index.js
@@ -0,0 +1,22 @@
+import { msft } from '../util/datasets';
+import showComponent from '../util/showComponent';
+
+window.onload = () => {
+  showComponent('LineChart', 'div', {
+    data: msft,
+    x: 'date',
+    y: ['price'],
+    width: 735,
+    height: 535,
+    hoverSize: 50,
+    padding: {
+      top: 20,
+      bottom: 45,
+      left: 45,
+      right: 20
+    },
+    showPoints: true,
+    pointSize: 25,
+    renderer: 'svg'
+  });
+};
