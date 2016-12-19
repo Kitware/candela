0d50f38019b00e7a65d1e721b2490819ce7fc2d0
Roni Choudhury <roni.choudhury@kitware.com>
2016-12-19 13:04:06 -0500

    test: fix preloader paths for coverage tests

--- config/webpack-coverage.config.js
+++ config/webpack-coverage.config.js
@@ -1,9 +1,19 @@
+var path = require('path');
+
 var config = require('./webpack-testing.config');
+
+var basePath = path.resolve(__dirname, '..');
 
 config.module.preLoaders = [
   {
     test: /\.js$/,
-    include: /src/,
+    include: [
+      basePath + '/index.js',
+      basePath + '/components',
+      basePath + '/test',
+      basePath + '/util',
+      basePath + '/VisComponent'
+    ],
     exclude: /(external|node_modules|test)/,
     loader: 'babel-istanbul-loader',
     query: {
