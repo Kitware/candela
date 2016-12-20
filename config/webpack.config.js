974fd99d983f7a090bd7f0d080adadccf5eb6e6f
Roni Choudhury <roni.choudhury@kitware.com>
2016-12-20 13:29:51 -0500

    build(webpack): Abstract loader paths into a variable

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,6 +5,19 @@ var CleanPlugin = require('clean-webpack-plugin');
 
 __dirname = path.resolve(__dirname, '..');
 
+var includePaths = [
+  __dirname + '/app',
+  __dirname + '/index.js',
+  __dirname + '/components',
+  __dirname + '/test',
+  __dirname + '/util',
+  __dirname + '/VisComponent'
+];
+
+function addNMPath(package, paths) {
+  return [new RegExp('/node_modules/' + package + '/')].concat(paths);
+}
+
 module.exports = {
   devtool: 'source-map',
   __dirname: __dirname,
@@ -30,46 +43,47 @@ module.exports = {
         query: {
           presets: ['es2015']
         },
-        include: [
-          __dirname + '/app',
-          __dirname + '/index.js',
-          __dirname + '/components',
-          __dirname + '/test',
-          __dirname + '/util',
-          __dirname + '/VisComponent'
-        ]
+        include: includePaths
       },
       {
         test: /\.jpe?g$|\.gif$|\.png$|\.woff$|\.wav$|\.mp3$|\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$|\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader'
+        loader: 'url-loader',
+        include: addNMPath('font-awesome', includePaths)
       },
       {
         test: /\.csv$/,
-        loader: 'raw-loader'
+        loader: 'raw-loader',
+        include: includePaths
       },
       {
         test: /\.html$/,
-        loader: 'html-loader?attrs=img:src'
+        loader: 'html-loader?attrs=img:src',
+        include: includePaths
       },
       {
         test: /\.styl$/,
-        loaders: ['style-loader', 'css-loader', 'stylus-loader']
+        loaders: ['style-loader', 'css-loader', 'stylus-loader'],
+        include: includePaths
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader']
+        loaders: ['style-loader', 'css-loader'],
+        include: addNMPath('LineUpJS', includePaths)
       },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        include: addNMPath('UpSet', includePaths)
       },
       {
         test: /\.jade$/,
-        loaders: ['jade-loader']
+        loaders: ['jade-loader'],
+        include: includePaths
       },
       {
         test: /\.json$/,
-        loaders: ['json-loader', 'strip-json-comments-loader']
+        loaders: ['json-loader', 'strip-json-comments-loader'],
+        include: addNMPath('datalib', includePaths)
       }
     ]
   }
