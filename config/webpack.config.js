5ce898de26607467e80434f420739d076663a206
Roni Choudhury <roni.choudhury@kitware.com>
2016-12-20 15:21:50 -0500

    build(webpack): abstract loader configuration into a standalone function

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,22 +3,11 @@ var path = require('path');
 
 var CleanPlugin = require('clean-webpack-plugin');
 
+var candelaWebpack = require('../webpack');
+
 __dirname = path.resolve(__dirname, '..');
 
-var includePaths = [
-  __dirname + '/app',
-  __dirname + '/index.js',
-  __dirname + '/components',
-  __dirname + '/test',
-  __dirname + '/util',
-  __dirname + '/VisComponent'
-];
-
-function addNMPath(package, paths) {
-  return [new RegExp('/node_modules/' + package + '/')].concat(paths);
-}
-
-module.exports = {
+module.exports = candelaWebpack({
   devtool: 'source-map',
   __dirname: __dirname,
   entry: {
@@ -34,57 +23,5 @@ module.exports = {
     new CleanPlugin([path.resolve(__dirname, 'build/candela')], {
       root: __dirname
     }),
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        },
-        include: includePaths
-      },
-      {
-        test: /\.jpe?g$|\.gif$|\.png$|\.woff$|\.wav$|\.mp3$|\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$|\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-        include: addNMPath('font-awesome', includePaths)
-      },
-      {
-        test: /\.csv$/,
-        loader: 'raw-loader',
-        include: includePaths
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader?attrs=img:src',
-        include: includePaths
-      },
-      {
-        test: /\.styl$/,
-        loaders: ['style-loader', 'css-loader', 'stylus-loader'],
-        include: includePaths
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-        include: addNMPath('LineUpJS', includePaths)
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: addNMPath('UpSet', includePaths)
-      },
-      {
-        test: /\.jade$/,
-        loaders: ['jade-loader'],
-        include: includePaths
-      },
-      {
-        test: /\.json$/,
-        loaders: ['json-loader', 'strip-json-comments-loader'],
-        include: addNMPath('datalib', includePaths)
-      }
-    ]
-  }
-};
+  ]
+}, __dirname);
---
+++ webpack.js
@@ -0,0 +1,88 @@
+function _includePaths (basePath) {
+  if (basePath) {
+    return [
+      basePath + '/app',
+      basePath + '/index.js',
+      basePath + '/components',
+      basePath + '/test',
+      basePath + '/util',
+      basePath + '/VisComponent'
+    ];
+  } else {
+    return [
+      /\/node_modules\/candela\//
+    ]
+  }
+}
+
+function addNMPath(packages, paths) {
+  if (!Array.isArray(packages)) {
+    packages = [packages];
+  }
+
+  return packages.map(function (package) {
+    return new RegExp('/node_modules/' + package + '/')
+  }).concat(paths);
+}
+
+module.exports = function (config, basePath) {
+  var includePaths = _includePaths(basePath);
+
+  // Install empty module and module.loaders entries if missing.
+  config.module = config.module || {};
+  config.module.loaders = config.module.loaders || [];
+
+  // Prepend the Candela loaders.
+  config.module.loaders = [
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      },
+      include: includePaths
+    },
+    {
+      test: /\.jpe?g$|\.gif$|\.png$|\.woff$|\.wav$|\.mp3$|\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$|\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url-loader',
+      include: addNMPath(['font-awesome', 'bootstrap'], includePaths)
+    },
+    {
+      test: /\.csv$/,
+      loader: 'raw-loader',
+      include: includePaths
+    },
+    {
+      test: /\.html$/,
+      loader: 'html-loader?attrs=img:src',
+      include: includePaths
+    },
+    {
+      test: /\.styl$/,
+      loaders: ['style-loader', 'css-loader', 'stylus-loader'],
+      include: includePaths
+    },
+    {
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader'],
+      include: addNMPath('LineUpJS', includePaths)
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: addNMPath('UpSet', includePaths)
+    },
+    {
+      test: /\.jade$/,
+      loaders: ['jade-loader'],
+      include: includePaths
+    },
+    {
+      test: /\.json$/,
+      loaders: ['json-loader', 'strip-json-comments-loader'],
+      include: addNMPath('datalib', includePaths)
+    }
+  ].concat(config.module.loaders);
+
+  return config;
+};
