385e9af5a89d4645e44831c6868371848b182442
Roni Choudhury <roni.choudhury@kitware.com>
2017-04-19 14:11:36 -0400

    fix(examples): update DOM element creation method to work for SVG

--- app/examples/util/showComponent/index.js
+++ app/examples/util/showComponent/index.js
@@ -1,13 +1,27 @@
 import html from './index.jade';
 import './index.styl';
 
+const createDOMElement = (eltType) => {
+  let ret = null;
+  switch (eltType) {
+  case 'svg':
+    ret = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    break;
+
+  default:
+    ret = document.createElement(eltType);
+  }
+
+  return ret;
+};
+
 export default function showComponent (Component, elementType, options) {
   document.body.innerHTML = html();
 
   let el = document.getElementById('vis-container')
-  .appendChild(document.createElement(elementType));
+    .appendChild(createDOMElement(elementType));
   el.setAttribute('id', 'vis-element');
-  el.className = 'vis-full';
+  el.setAttribute('class', 'vis-full');
 
   let vis = new Component(el, options);
   vis.render();
