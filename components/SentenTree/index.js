d106c7393bab86142a1292bcab6cd05d5e7ec97e
Roni Choudhury <roni.choudhury@kitware.com>
2017-04-28 18:21:50 -0400

    refactor(SentenTree): add options spec

--- components/SentenTree/index.js
+++ components/SentenTree/index.js
@@ -3,6 +3,46 @@ import { SentenTreeBuilder,
          SentenTreeVis } from 'sententree/dist/SentenTree';
 
 export default class SentenTree extends VisComponent {
+  static get options () {
+    return [
+      {
+        name: 'data',
+        description: 'The data table.',
+        type: 'table',
+        format: 'objectlist'
+      },
+      {
+        name: 'id',
+        description: 'The field containing the identifier of each row.',
+        type: 'string',
+        domain: {
+          mode: 'field',
+          from: 'data',
+          fieldTypes: ['string', 'integer', 'number']
+        }
+      },
+      {
+        name: 'text',
+        description: 'The field containing the text sample.',
+        domain: {
+          mode: 'field',
+          from: 'data',
+          fieldTypes: ['string']
+        }
+      },
+      {
+        name: 'count',
+        description: 'The field containing the count for each text sample.',
+        type: 'string',
+        domain: {
+          mode: 'field',
+          from: 'data',
+          fieldTypes: ['integer']
+        }
+      }
+    ];
+  }
+
   constructor (el, {data, graphs = 3}) {
     super(el);
 
