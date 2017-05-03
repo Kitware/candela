7635ea174239f8267c6dffcb96656b21acc2e099
Roni Choudhury <roni.choudhury@kitware.com>
2017-05-03 14:24:26 -0400

    style: fix linting errors

--- components/Glo/index.js
+++ components/Glo/index.js
@@ -1,14 +1,14 @@
 import 'glo/js/namespace';
-import 'glo/js/coordinates.js'
-import 'glo/js/node_group.js'
-import 'glo/js/canvas.js'
-import 'glo/js/node_generation.js'
-import 'glo/js/edge_generation.js'
-import 'glo/js/helpers.js'
-import 'glo/js/glo.js'
-import 'glo/js/api.js'
-import 'glo/js/techniques.js'
-import 'glo/js/figures.js'
+import 'glo/js/coordinates.js';
+import 'glo/js/node_group.js';
+import 'glo/js/canvas.js';
+import 'glo/js/node_generation.js';
+import 'glo/js/edge_generation.js';
+import 'glo/js/helpers.js';
+import 'glo/js/glo.js';
+import 'glo/js/api.js';
+import 'glo/js/techniques.js';
+import 'glo/js/figures.js';
 
 import VisComponent from '../../VisComponent';
 
@@ -20,6 +20,8 @@ export default class Glo extends VisComponent {
   }
 
   render () {
+    const GLO = window.GLO;
+
     console.log('Glo.render()');
     console.log('GLO object', GLO);
   }
