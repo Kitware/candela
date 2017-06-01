e76c1a1bf8ea9ad649bd346a6e39f04784636fd5
Roni Choudhury <roni.choudhury@kitware.com>
2017-06-01 17:07:22 -0400

    refactor: create candela-glo microlibrary with example

--- examples/datasets/index.js
+++ examples/datasets/index.js
@@ -5,6 +5,8 @@ import dl from 'datalib';
 import heatmap_raw from './heatmap.csv';
 import hmohiv_raw from './hmohiv.csv';
 import iris from './iris.json';
+import lesmis_nodes_raw from './lesmis_nodes.csv';
+import lesmis_edges_raw from './lesmis_edges.csv';
 import msft_raw from './msft.csv';
 import simpsons_raw from './simpsons.csv';
 import simpsons_alternate_raw from './simpsons-alternate.csv';
@@ -25,6 +27,10 @@ const readTSV = raw => readDSV(raw, 'tsv');
 const goal = readTSV(goal_raw);
 const heatmap = readCSV(heatmap_raw);
 const hmohiv = readCSV(hmohiv_raw);
+const lesmis = {
+  nodes: readCSV(lesmis_nodes_raw),
+  edges: readCSV(lesmis_edges_raw)
+};
 const msft = readCSV(msft_raw);
 const simpsons = readCSV(simpsons_raw);
 const simpsons_alternate = readCSV(simpsons_alternate_raw);
@@ -35,6 +41,7 @@ export {
   heatmap,
   hmohiv,
   iris,
+  lesmis,
   msft,
   simpsons,
   simpsons_alternate,
---
+++ packages/candela-glo/index.js
@@ -0,0 +1,5 @@
+import candela from 'candela';
+
+import Glo from './Glo';
+
+candela.registerComponent('Glo', Glo);
