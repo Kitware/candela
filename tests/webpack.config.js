686808da1d9d8216e28c4393d5c19f85bd4eff1a
Roni Choudhury <roni.choudhury@kitware.com>
2017-06-05 14:57:01 -0400

    test: use a more standard format for webpack config

--- tests/webpack.config.js
+++ tests/webpack.config.js
@@ -1,33 +1,24 @@
-// var webpack = require('webpack');
 var candelaWebpack = require('../webpack');
 
-var config = {};
-
-config.entry = {
-  'tests': './tests.js'
-};
-
-config.devtool = 'inline-source-map';
-
-config.node = {
-  fs: 'empty'
-};
-
-config.output = {
-  path: 'build',
-  filename: 'tests.js'
-};
-config.module = {
-  loaders: [
-    {
-      test: /\.css/,
-      loaders: ['style-loader', 'css-loader'],
-      include: /node_modules\/nvd3/,
-    },
-  ]
-};
-delete config.plugins;
-
-var final = candelaWebpack(config);
-
-module.exports = final;
+module.exports = candelaWebpack({
+  entry: {
+    tests: './tests.js'
+  },
+  devtool: 'inline-source-map',
+  node: {
+    fs: 'empty'
+  },
+  output: {
+    path: 'build',
+      filename: 'tests.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader'],
+        include: /node_modules\/nvd3/,
+      },
+    ]
+  }
+});
