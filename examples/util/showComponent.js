312268e933202fadc02bea8911a2606745508444
Roni Choudhury <roni.choudhury@kitware.com>
2017-06-06 17:25:30 -0400

    test: make image tests work again

--- examples/util/showComponent.js
+++ examples/util/showComponent.js
@@ -1,5 +1,9 @@
+import './index.styl';
+
 const showComponent = (Component, options) => {
   const div = document.createElement('div');
+  div.setAttribute('id', 'vis-element');
+
   document.body.appendChild(div);
 
   return new Component(div, options);
--- packages/candela-vega/LineChart/test/linechart-points.image.js
+++ packages/candela-vega/LineChart/test/linechart-points.image.js
@@ -1,8 +1,8 @@
-import imageTest from '../../../util/imageTest';
+import imageTest from '../../../../util/imageTest';
 
 imageTest({
   name: 'linechart-points',
-  url: 'http://localhost:28000/examples/linechart-points',
+  url: 'http://localhost:28000/examples/line-points',
   selector: '#vis-element',
   delay: 1000,
   threshold: 0.001
