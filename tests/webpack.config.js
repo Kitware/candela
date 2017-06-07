a7a2ba068788b22f4c28a5b48557d85431fa2923
Roni Choudhury <roni.choudhury@kitware.com>
2017-06-07 11:53:41 -0400

    test: unify webpack configs

--- tests/webpack.config.js
+++ tests/webpack.config.js
@@ -1,14 +1,39 @@
 var candelaWebpack = require('../webpack');
 
-module.exports = candelaWebpack({
+var config = {
   entry: {
     tests: './tests.js'
   },
   output: {
     path: '../build/tests',
-    filename: 'unit.js'
+    filename: null
   },
+  module: null,
   node: {
     fs: 'empty'
   }
-});
+};
+
+if (process.env.CANDELA_COVERAGE) {
+  config.output.filename = 'coverage.js';
+  config.module = {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        include: [
+          /node_modules\/candela\//
+        ],
+        exclude: /\/test\//,
+        loader: 'babel-istanbul-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  };
+} else {
+  config.output.filename = 'unit.js';
+  delete config.module;
+}
+
+module.exports = candelaWebpack(config);
