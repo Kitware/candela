2f6d2d59dadc5258e1ed983418a597b4ea07f17b
Roni Choudhury <roni.choudhury@kitware.com>
2017-06-07 16:04:57 -0400

    test: refactor micropackage testing strategy into a function

--- packages/candela-geojs/test/exports.js
+++ packages/candela-geojs/test/exports.js
@@ -1,22 +1,6 @@
-import test from 'tape-catch';
+import exportTest from 'candela/util/exportTest';
 
-import candela from 'candela';
-
-const components = [
+exportTest('candela-geojs', [
   'Geo',
   'GeoDots'
-];
-
-test('Contents of candela-geojs micropackage', t => {
-  candela.unregisterAll();
-  require('candela-geojs');
-
-  t.equal(Object.keys(candela.components).length, components.length, `candela-geojs exports ${components.length} component${components.length > 1 ? 's' : ''}`);
-
-  components.forEach(comp => {
-    t.ok(candela.components[comp], `candela.components.${comp} exists`);
-    t.equal(typeof candela.components[comp], 'function', `candela.components.${comp} is a function`);
-  });
-
-  t.end();
-});
+]);
