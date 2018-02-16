041f137337878807f478f3abe134a6064d4cd0da
Roni Choudhury <roni.choudhury@kitware.com>
2018-02-16 12:57:09 -0500

    ci(test): add a transparent bg barchart baseline

--- plugins/vega/BarChart/test/barchart.image.js
+++ plugins/vega/BarChart/test/barchart.image.js
@@ -2,7 +2,10 @@ import imageTest from '../../../../test/util/imageTest';
 
 imageTest({
   name: 'barchart',
-  extraBaselines: ['barchart-circle'],
+  extraBaselines: [
+    'barchart-circle',
+    'barchart-circle2'
+  ],
   url: 'http://localhost:28000/examples/bar',
   selector: '#vis-element',
   delay: 1000,
