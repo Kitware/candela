0b7ae9006b8250f5bc94b2a755dcc27bacf03c9b
Roni Choudhury <roni.choudhury@kitware.com>
2018-04-20 16:19:09 -0400

    refactor: move lineup plugin to package

--- packages/lineup/src/LineUp/index.js
+++ packages/lineup/src/LineUp/index.js
@@ -1,4 +1,4 @@
-import VisComponent from 'candela/VisComponent';
+import { VisComponent } from '@candela/core';
 
 import $ from 'jquery';
 import datalib from 'datalib';
--- plugins/lineup/load.js
+++
@@ -1,5 +0,0 @@
-import candela from 'candela';
-
-import { LineUp } from '.';
-
-candela.register(LineUp, 'LineUp');
