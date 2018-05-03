86642f4719f95183411eac1f1cfef7be2f38adf0
Roni Choudhury <roni.choudhury@kitware.com>
2018-05-03 11:05:19 -0400

    test: add all remaining image tests to examples package

--- packages/examples/test/index.js
+++ packages/examples/test/index.js
@@ -101,3 +101,79 @@ imageTest({
   delay: 4000,
   threshold: 0.001
 });
+
+imageTest({
+  name: 'geo',
+  dir: 'geo',
+  url: 'http://localhost:28000/geoblank',
+  selector: '#vis-element',
+  threshold: 0.001,
+  verbose: true
+});
+
+imageTest({
+  name: 'geodots',
+  dir: 'geo',
+  url: 'http://localhost:28000/geodotsblank',
+  selector: '#vis-element',
+  threshold: 0.001
+});
+
+imageTest({
+  name: 'lineup',
+  dir: 'lineup',
+  extraBaselines: ['lineup-circle'],
+  url: 'http://localhost:28000/lineup',
+  selector: '#vis-element',
+  delay: 2000,
+  threshold: 0.001
+});
+
+imageTest({
+  name: 'distributionplot',
+  dir: 'distributionplot',
+  extraBaselines: ['distributionplot-circle'],
+  url: 'http://localhost:28000/distribution',
+  selector: '#vis-element',
+  delay: 1000,
+  threshold: 0.001
+});
+
+imageTest({
+  name: 'distributionplot-bimodal',
+  dir: 'distributionplot',
+  extraBaselines: ['distributionplot-bimodal-circle'],
+  url: 'http://localhost:28000/distribution-bimodal',
+  selector: '#vis-element',
+  delay: 1000,
+  threshold: 0.001
+});
+
+imageTest({
+  name: 'normalprobabilityplot',
+  dir: 'normalprobabilityplot',
+  extraBaselines: ['normalprobabilityplot-circle'],
+  url: 'http://localhost:28000/normalprobability',
+  selector: '#vis-element',
+  delay: 1000,
+  threshold: 0.001
+});
+
+imageTest({
+  name: 'normalprobabilityplot-bimodal',
+  dir: 'normalprobabilityplot',
+  extraBaselines: ['normalprobabilityplot-bimodal-circle'],
+  url: 'http://localhost:28000/normalprobability-bimodal',
+  selector: '#vis-element',
+  delay: 1000,
+  threshold: 0.001
+});
+
+// imageTest({
+  // name: 'trackerdash',
+  // dir: 'trackerdash',
+  // url: 'http://localhost:28000/trackerdash',
+  // selector: '.trackerdash',
+  // delay: 2000,
+  // threshold: 0.02
+// });
