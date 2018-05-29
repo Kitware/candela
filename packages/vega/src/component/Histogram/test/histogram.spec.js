e67fe67246170aa4a30a65a8a8664533754aaccf
Roni Choudhury <roni.choudhury@kitware.com>
2018-05-29 16:30:24 -0400

    test(vega): add unit test for histogram

---
+++ packages/vega/src/component/Histogram/test/histogram.spec.js
@@ -0,0 +1,32 @@
+import { select } from 'd3-selection';
+import test from 'tape-catch';
+
+import Histogram from '..';
+
+test('Histogram component', t => {
+  const data = [...Array(200).keys()].map(i => ({
+    a: Math.floor(i / 10)
+  }));
+  const width = 400;
+  const height = 400;
+  const maxBins = 10;
+
+  let el = document.createElement('div');
+  let vis = new Histogram(el, {
+    data,
+    x: 'a',
+    maxBins,
+    width,
+    height,
+    renderer: 'svg'
+  });
+  vis.render();
+
+  const bars = select(vis.el)
+    .select('.mark-rect.role-mark.marks')
+    .selectAll('path')
+    .nodes();
+  t.equal(bars.length, maxBins, 'Plot has correct number of bin marks');
+
+  t.end();
+});
